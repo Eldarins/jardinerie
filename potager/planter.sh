16c2c6fa8959437fcb7fb9eb4b8124e4c918e643
startakim64 <adib.karim64@gmail.com>
2021-05-25 11:36:26 -0400

    change line planter.sh

--- potager/planter.sh
+++ potager/planter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Planter des choux"
+echo "Planter des choux c'est bien mais planter des carottes c'est mieux"
 echo "Avec les coudes"
 echo "pierre qui roule n'ammasse pas mousse"
+
